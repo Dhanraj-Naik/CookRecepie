445809b8dfa836a99845d9d4c7b68b535689d730
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2024-02-21 12:49:56 +0530

    -added svg package for handling svg images

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,18 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+// Your current configuration
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Configuration for handling SVG files
+const svgConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+  },
+};
+
+// Merge the SVG configuration with the current configuration
+module.exports = mergeConfig(defaultConfig, svgConfig);
